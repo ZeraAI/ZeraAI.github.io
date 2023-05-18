8befd69d26b9aac49f3a515b89abcbda54fc45ba
Ubuntu <azureuser@zen.jbsjdw1yd1hurn2ob2c55l14hb.fx.internal.cloudapp.net>
2023-05-18 18:59:23 +0000

    website updated with react

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Who Am I?</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Presenting a highly effective communicator and strategic leader with a genuine passion for delivering remarkable user experiences. With a teaching background, I possess the unique ability to simplify intricate concepts and connect with diverse audiences effortlessly. Attention to detail is my strength, reflected in meticulous and impactful written communication. I excel in building networks across industries, fostering lasting relationships and promoting collaborative success. Get ready to experience a dedicated professional who seamlessly combines expertise, innovation, and unwavering commitment to customer satisfaction.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,386 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  homepage,
+  teashop, 
+  formatcamera, 
+  pinkcalculator, 
+  goldstarsurvey, 
+  kittenwebsite,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "User Experience Advocate",
+    icon: web,
+  },
+  {
+    title: "Educator",
+    icon: mobile,
+  },
+  {
+    title: "Strategic Partnership Developer",
+    icon: backend,
+  },
+  {
+    title: "Communication Specialist",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Azure",
+    icon: "https://media.licdn.com/dms/image/C4D07AQFzZ4nSNaZMHg/group-logo_image-shrink_92x92/0/1630997456959?e=1685041200&v=beta&t=Ev72NkFVFiU940jGb8WUCSLxue8HPIF80qrmDPmIaEE",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "C#",
+    icon: "https://media.licdn.com/dms/image/C4D07AQHmN1_fJVToBw/group-logo_image-shrink_92x92/0/1631373638658?e=1685041200&v=beta&t=yu-yMARMa57m1-pA38KgKi0OH3dfVklZPJfKYVLlca4",
+  },
+  {
+    name: "Three JS",
+    icon: "https://media.licdn.com/dms/image/D4E07AQEsKBDidLaKwA/group-logo_image-shrink_92x92/0/1659425140396?e=1685041200&v=beta&t=0sbx3uJkDibo59lVDlw_nwxBpHnT0lKrorxkyCB5deg",
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "Ubuntu",
+    icon: "https://media.licdn.com/dms/image/C560BAQEbIYAkAURcYw/company-logo_200_200/0/1650566106882?e=1692230400&v=beta&t=-EKl9scc2MidRje09svyp9JZEo561HCZC0-k4VNHOFE",
+  },
+];
+
+const experiences = [
+  {
+    title: "Frontend Development Partner",
+    company_name: "Richview",
+    icon: "https://media.licdn.com/dms/image/C4E0BAQGhI8PRvyM7Rg/company-logo_100_100/0/1676227647227?e=1692230400&v=beta&t=j7nuoUg6h9KOJ8bq3YkbKALOIbSbw9rnZ6_-Z6C3T8o",
+    iconBg: "#383E56",
+    date: "April 2023 - Present",
+    points: [
+      "Developing and maintaining AI web applications using Next.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Work through software lifecycle through Azure DevOps and cloud.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Frontend Fullstack Developer",
+    company_name: "Mohn Technology",
+    icon: "https://media.licdn.com/dms/image/C4E0BAQE6E7ex90I9JQ/company-logo_100_100/0/1592485032734?e=1692230400&v=beta&t=MhNjqo0uk5ag9BlrJNVf78F8PjN1CWrpo3DLwgpu4x4",
+    iconBg: "#E6DEDD",
+    date: "Oct 2022 - June 2023",
+    points: [
+      "Specialize in frontend web development using Vue.js, HTML, CSS, and JavaScript",
+      "Implement responsive design, optimize for performance and user experience",
+      "Backend technology Golang and Docker to develop efficient, scalable, and secure web applications",
+      "Collaborate closely with 3-5 developers at a time",
+      "Scrum and Kanban methodologies",
+      "Stay up to date with emerging trends and technologies in web development, conduct internal training sessions to share knowledge and best practices with colleagues",
+    ],
+  },
+  {
+    title: "Web Development Consultant",
+    company_name: "RobustSys",
+    icon: "https://media.licdn.com/dms/image/C4D0BAQFpZKmOkbnHCg/company-logo_100_100/0/1679904327467?e=1692230400&v=beta&t=SsMiTUEbKh6rkfy7HRAV27ln4MBPdjQBTHCTvUGXQmw",
+    iconBg: "#383E56",
+    date: "Mar 2023 - Apr 2023",
+    points: [
+      "Trained team on latest web development technologies, including SAPUI5 and Vue.js",
+      "Demonstrated excellent communication and presentation skills",
+    ],
+  },
+  {
+    title: "Mobile Engineer (Quality Assurance focus)",
+    company_name: "Samsung",
+    icon: "https://media.licdn.com/dms/image/C560BAQFsvADfvcJi_A/company-logo_100_100/0/1676336400130?e=1692230400&v=beta&t=2RdrTqGuxpig3-zm5fjFdiApIwoeTtCqhbPsrTfaPzE",
+    iconBg: "#E6DEDD",
+    date: "Aug 2022 - Oct 2022",
+    points: [
+      "Closely worked together with VIP at Samsung to bring new ideas and discuss best practices",
+      "Developed and maintained together with team cutting-edge mobile applications with Scrum methodology",
+      "Took initiative to learn Vue.js and other technologies on my own, and incorporate into work to enhance quality and functionality of existing applications",
+      "Showed team automation techniques such as UI Automator, resulting in significant time and cost savings",
+      "Utilized Samsung’s SDS for version control, code review, and collaboration",
+    ],
+  },
+  {
+    title: "Git Version Control Team Lead",
+    company_name: "Texas Instruments",
+    icon: "https://media.licdn.com/dms/image/C4D0BAQHjRIWKaz-XKQ/company-logo_100_100/0/1590788980221?e=1692230400&v=beta&t=6WS_Hp46izo0QIt1X35ffhynnmP9ouwwqN22q4HS3lQ",
+    iconBg: "#383E56",
+    date: "Jan 2022 - May 2022",
+    points: [
+      "Led team to build on top of Texas Instrument’s version control",
+      "Successful completion of project and given recommendation at Texas Instruments.",
+    ],
+  },
+  {
+    title: "Frontend Web Development Instructor (including freelance)",
+    company_name: "Rex Academy",
+    icon: "https://media.licdn.com/dms/image/C560BAQHiAfLfZvl1EA/company-logo_100_100/0/1598340014995?e=1692230400&v=beta&t=jk05J91w1fO-_0gqpkaV8DVxwBAbdSuYgoRLByHqOUg",
+    iconBg: "#E6DEDD",
+    date: "Aug 2019 - Dec 2020",
+    points: [
+      " Prior to Rex Academy, I have had years of experience in education (professional and freelance)",
+      "Passionate about teaching web development, empowering all individuals to succeed",
+      "Continue to provide private instruction to individuals seeking to improve even today",
+    ],
+  },
+  {
+    title: "Accessibility & Teaching Assistance",
+    company_name: "Arlington Independent School District",
+    icon: "https://media.licdn.com/dms/image/C4D0BAQG0lbikZ1Bs9w/company-logo_100_100/0/1519895072821?e=1692230400&v=beta&t=z6ijBWuR8bsgnNq3Y_XaLt1M2Agg196IXlPbTh_g-dE",
+    iconBg: "#383E56",
+    date: "Aug 2016 - Jul 2018",
+    points: [
+      "Highly values the importance of accessibility",
+      "Worked with screen readers, text-to-speech, keyboard navigation, high contrast mode",
+      "Talked with clients and end-users to understand specific needs",
+      "I have a unique perspective on the importance of accessibility, especially in web design, and I am committed to creating websites accessible to all users, regardless of abilities or disabilities",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I was very impressed with Zen's thoughtfulness and decision making to make sure everyone felt involved. He knows how to listen and follow people. This sort of quality makes him a natural leader.",
+    name: "Thanh Nguyen",
+    designation: "Developer",
+    company: "Texas Instruments",
+    image: "https://media.licdn.com/dms/image/C5603AQEz3IeRNOD-SQ/profile-displayphoto-shrink_400_400/0/1639373628437?e=1689811200&v=beta&t=KspmA4IOjqw9Efc2zh7zd0zEzEvCkqfhtUEjOzR52CI",
+  },
+  {
+    testimonial:
+      "Zen is among the best students I’ve had the pleasure of interacting with in my ten years of education. He has earned the admiration of his peers, educators, and administrators. I am confident he is going to achieve great things.",
+    name: "Jeff Casper",
+    designation: "Professor",
+    company: "Dallas College",
+    image: "https://pbs.twimg.com/profile_images/1473369043109355530/C05pMsyB_400x400.jpg",
+  },
+  {
+    testimonial:
+      "Zen has an unbridled enthusiasm for learning, and is constantly working to improve his technical skills. However, there is more to Zen than his intellect. He has a uniquely compassionate outlook on cultural and societal issues. Zen is someone who can provide leadership for a team as well as accomplish things on his own.",
+    name: "Karen Mazidi",
+    designation: "Machine Learning Professor",
+    company: "The University of Texas at Dallas",
+    image: "https://media.licdn.com/dms/image/C4E03AQH9BWgSGbgChw/profile-displayphoto-shrink_400_400/0/1609876702961?e=1689811200&v=beta&t=ADMXr40PiIudLwMZEmvqPruz2AwdQmPrHWBwVDAuo4o",
+  },
+  {
+    testimonial:
+      "I wholeheartedly recommend Zen based on his exceptional communication skills, adeptly conveying complex ideas to diverse audiences. Zen consistently prioritizes exceptional user experiences, fostering intuitive and enjoyable interactions. His remarkable ability to build valuable partnerships across industries is an invaluable asset.",
+    name: "Alan Ng",
+    designation: "Co-Founder",
+    company: "Gullover",
+    image: "/src/assets/alanng.jpg",
+  },
+  {
+    testimonial:
+      "Zen's strong work ethic and years of public experience truly set him apart. His dedication to delivering high-quality content and ability to adapt to different subjects and industries are qualities I deeply respect. With Zen's exceptional skills and commitment to producing outstanding written work, I have no doubt that he will continue to excel and make a significant impact.",
+    name: "Ankit Khare",
+    designation: "Developer Relations Leader",
+    company: "Twelve Labs",
+    image: "https://media.licdn.com/dms/image/D5603AQFOx8wRV3iwgA/profile-displayphoto-shrink_400_400/0/1680668887983?e=1689811200&v=beta&t=D5-H7VBnQif2VBUdol4PNnKfpKDjS01nyYgs9m_eMcY",
+  },
+  {
+    testimonial:
+      "Meow meow meow meow. Meow? Meow meow! Meow meow... meow. (translation: He treats me very well and gives me lots of wet food and treats)",
+    name: "Dr. Rudolf",
+    designation: "Doctor Feline",
+    company: "Meowversity of Technology",
+    image: "/src/assets/Rudolf.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "This Website",
+    description:
+      "My 3D personal website built to showcase my personality. The website is hosted through GitHub with the domain provided by Namecheap. Have you tried rotating the plane?",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "threejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: homepage,
+    source_code_link: "https://github.com/ZeraAI/ZeraAI.github.io",
+    live_demo_link: "https://zenpark.co",
+  },
+  {
+    name: "Tea Shop in Bergen, Norway",
+    description:
+      "User friendly and built with wireframes. High ticket sales pitch to increase conversion rate.",
+    tags: [
+      {
+        name: "responsive on mobile",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "pure html/css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: teashop,
+    source_code_link: "https://github.com/ZeraAI/tea-shops-in-bergen",
+    live_demo_link: "https://bergenteashop.netlify.app/",
+  },
+  {
+    name: "Automatic Camera",
+    description:
+      "Very nice-looking camera website",
+    tags: [
+      {
+        name: "realistic",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nice to look at",
+        color: "green-text-gradient",
+      },
+    ],
+    image: formatcamera,
+    source_code_link: "https://github.com/ZeraAI/Fotomatic-website",
+    live_demo_link: "https://fotomatic-camera.netlify.app/",
+  },
+  {
+    name: "The Pink Calculator",
+    description:
+      "Very nice-looking calculator",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "gradient colors",
+        color: "green-text-gradient",
+      },
+    ],
+    image: pinkcalculator,
+    source_code_link: "https://github.com/ZeraAI/The-Pink-Calculator",
+    live_demo_link: "https://thepinkcalculator.netlify.app/",
+  },
+  {
+    name: "Survey Website",
+    description:
+      "A survey website built as a challenge, built responsively for iPhone 6 and Google Pixel models.",
+    tags: [
+      {
+        name: "responsive on mobile",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "pure html/css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "glowing star rating system",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: goldstarsurvey,
+    source_code_link: "https://github.com/ZeraAI/survey-website-for-feedback",
+    live_demo_link: "https://feedbacksurvey.netlify.app/",
+  },
+  {
+    name: "Website for Kittens",
+    description:
+      "A special website built for cat lovers, created by ChatGPT to test its capabilities",
+    tags: [
+      {
+        name: "GPT-generated",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "cute",
+        color: "green-text-gradient",
+      },
+    ],
+    image: kittenwebsite,
+    source_code_link: "https://github.com/ZeraAI/website-dedicated-to-cats",
+    live_demo_link: "https://thecatwebsite.netlify.app/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
